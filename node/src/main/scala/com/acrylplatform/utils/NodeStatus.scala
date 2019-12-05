8582f78aedb51fafca8dd8bc1a8e2ea47d7e4f1c
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-12-05 15:39:08 +0700

    Code style

--- node/src/main/scala/com/acrylplatform/utils/NodeStatus.scala
+++ node/src/main/scala/com/acrylplatform/utils/NodeStatus.scala
@@ -22,79 +22,79 @@ object NodeStatus extends ScorexLogging {
 
   def start(enable: Boolean, blockchain: Blockchain, wallet: Wallet, network: NS, localAddress: Option[InetAddress]): Cancelable =
     scheduler.scheduleAtFixedRate(0.seconds, 120.seconds) {
-        val pid = getRuntimeMXBean.getName.split("@")(0)
+      val pid = getRuntimeMXBean.getName.split("@")(0)
 
-        def networkConnectionStatus: String =
-          Try {
-            List("https://ya.ru/", "https://yahoo.com/")
-              .map(url => Source.fromURL(url).mkString)
-              .filter(data => !data.isEmpty)
-          } match {
-            case Success(value) => if (value.isEmpty) "Failed" else "OK"
-            case Failure(_)     => "Failed"
-          }
-
-        def syncStatus: String = {
-          val heightLocal  = blockchain.height
-
-          val heightRemote = Try {
-            val json = Json.parse(get("https://nodes.acrylplatform.com/blocks/height"))
-            (json \ "height").get.toString()
-          } match {
-            case Success(value) => value.toInt
-            case Failure(_) => 0
-          }
-
-          if (heightRemote != heightLocal)
-            s"($heightLocal/$heightRemote)"
-          else
-            s"($heightLocal)"
-        }
-
-        def checkBalance(address: Address): Double =
-          blockchain.balance(address) / 100000000
-
-        val (nodeAddress, balance, effectiveBalance, effectiveBalanceNoConfirmations) =
-          if (wallet.privateKeyAccounts.isEmpty)
-            ("Unknown", "Unknown", "Unknown", "Unknown")
-          else {
-            val addr                      = wallet.privateKeyAccounts.head.toAddress
-            val balance                   = checkBalance(addr)
-            val effBalance                = blockchain.effectiveBalance(addr, 1000)
-            val effBalanceNoConfirmations = blockchain.effectiveBalance(addr, 0)
-            (addr, balance, effBalance, effBalanceNoConfirmations)
-          }
-
-        val disk = Try {
-          val file  = new File("/")
-          val total = (file.getTotalSpace / 1000000000).floor + "GB"
-          val used  = ((file.getTotalSpace - file.getUsableSpace) / 1000000000).floor + "GB"
-          val free  = (file.getUsableSpace / 1000000000).floor + "GB"
-          total + "/" + used + "/" + free
+      def networkConnectionStatus: String =
+        Try {
+          List("https://ya.ru/", "https://yahoo.com/", "https://google.com")
+            .map(url => get(url))
+            .filter(data => !data.isEmpty)
         } match {
-          case Success(value) => value
-          case Failure(_)     => "Unknown"
+          case Success(value) => if (value.isEmpty) "Failed" else "OK"
+          case Failure(_)     => "Failed"
         }
 
-        val internalIP = localAddress match {
-          case Some(value) => value.getHostAddress
-          case None        => "Unknown"
-        }
+      def syncStatus: String = {
+        val heightLocal = blockchain.height
 
-        val remoteVersion = Try {
-          val json = Json.parse(get("https://nodes.acrylplatform.com/node/version"))
-          (json \ "version").get.toString()
+        val heightRemote = Try {
+          val json = Json.parse(get("https://nodes.acrylplatform.com/blocks/height"))
+          (json \ "height").get.toString()
         } match {
-          case Success(value) => value
-          case Failure(_) => "Unknown"
+          case Success(value) => value.toInt
+          case Failure(_)     => 0
         }
 
-        val version = Version.VersionString + " " + s"($remoteVersion)"
+        if (heightRemote != heightLocal)
+          s"($heightLocal/$heightRemote)"
+        else
+          s"($heightLocal)"
+      }
 
-        val currentTimestamp = java.time.LocalDate.now.toString + " " + java.time.LocalTime.now.toString
+      def checkBalance(address: Address): Double =
+        blockchain.balance(address) / 100000000
 
-        //noinspection ScalaStyle
-        println(s"""
+      val (nodeAddress, balance, effectiveBalance, effectiveBalanceNoConfirmations) =
+        if (wallet.privateKeyAccounts.isEmpty)
+          ("Unknown", "Unknown", "Unknown", "Unknown")
+        else {
+          val addr                      = wallet.privateKeyAccounts.head.toAddress
+          val balance                   = checkBalance(addr)
+          val effBalance                = blockchain.effectiveBalance(addr, 1000)
+          val effBalanceNoConfirmations = blockchain.effectiveBalance(addr, 0)
+          (addr, balance, effBalance, effBalanceNoConfirmations)
+        }
+
+      val disk = Try {
+        val file  = new File("/")
+        val total = (file.getTotalSpace / 1000000000).floor + "GB"
+        val used  = ((file.getTotalSpace - file.getUsableSpace) / 1000000000).floor + "GB"
+        val free  = (file.getUsableSpace / 1000000000).floor + "GB"
+        total + "/" + used + "/" + free
+      } match {
+        case Success(value) => value
+        case Failure(_)     => "Unknown"
+      }
+
+      val internalIP = localAddress match {
+        case Some(value) => value.getHostAddress
+        case None        => "Unknown"
+      }
+
+      val remoteVersion = Try {
+        val json = Json.parse(get("https://nodes.acrylplatform.com/node/version"))
+        (json \ "version").get.toString()
+      } match {
+        case Success(value) => value
+        case Failure(_)     => "Unknown"
+      }
+
+      val version = Version.VersionString + " " + s"($remoteVersion)"
+
+      val currentTimestamp = java.time.LocalDate.now.toString + " " + java.time.LocalTime.now.toString
+
+      //noinspection ScalaStyle
+      println(s"""
                    |Node status: Running (pid $pid)
                    |Initialization: OK
                    |Network connection: $networkConnectionStatus
@@ -108,7 +108,7 @@ object NodeStatus extends ScorexLogging {
                    |Node version: $version
                    |Message generated on $currentTimestamp
                    |""".stripMargin)
-      }
+    }
 
   def stop(): Unit = scheduler.shutdown()
 
